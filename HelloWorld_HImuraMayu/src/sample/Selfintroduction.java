e161e3bf22d234cca3d86d8875175e8d45bf7b83
himuramayu <himuramayu@localhost>
2025-09-12 21:34:12 +0900

    2.3 Java導入

---
+++ HelloWorld_HImuraMayu/src/sample/Selfintroduction.java
@@ -0,0 +1,32 @@
+package sample;
+
+public class Selfintroduction {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+	
+	// 氏名
+	String name = "日村 真唯";
+	// 年齢
+	int age = 25;
+	// 趣味
+	String hobby = "小説を書く";
+	
+	
+	/*
+	 * 自己紹介を出力する処理
+	 * 文字列名 = "日村 真唯";
+	 * 整数 年齢 = 25;
+	 * 文字列趣味 = "小説を書く"; 
+	*/
+	
+	
+	System.out.println("私の名前は" + name + "です。");
+	System.out.println("年齢は" + age + "歳です。");
+	System.out.println("趣味は" + hobby + "ことです。" );
+	
+	
+	
+	}
+
+}
