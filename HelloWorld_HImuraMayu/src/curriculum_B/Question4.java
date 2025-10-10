25ef9aafb4efdaa65876d92f013099025952389e
himuramayu <himuramayu@localhost>
2025-10-11 06:54:42 +0900

    2.17 多次元配列

--- HelloWorld_HImuraMayu/src/curriculum_B/Question4.java
+++ HelloWorld_HImuraMayu/src/curriculum_B/Question4.java
@@ -1,14 +1,11 @@
 package curriculum_B;
 
 import java.util.Arrays;
-import java.util.Scanner;
 
 public class Question4 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-		
-		
 		
 	// Q1.
 		// int 型の配列 を作成し、1, 2, 3, 4, 5 を格納する
@@ -58,7 +55,7 @@ public class Question4 {
 		// 配列のすべての要素を 2 倍 し、結果を表示
 		// 結果を表示する際の処理に拡張for文を使用してください
 		
-		int[] counts = {1, 2, 3, 4, 5};
+		/*int[] counts = {1, 2, 3, 4, 5};
 		
 		// すべての要素を2倍にする（普通のfor文で処理）
 		for(int i= 0; i<counts.length; i++) {
@@ -99,10 +96,89 @@ public class Question4 {
 		    }else {
 				System.out.println(input+ " は配列に含まれていません。 ");
 			}
-        
-		sc.close();
+		sc.close(); */
+		
+		System.out.println("次はQ7です");
+		
+	// Q7.
+		// 2次元配列 int[][] array = {{1, 2}, {3, 4}, {5, 6}}; を用意する
+		// 配列の すべての要素を表示 
+		int[][] array = {
+				{1, 2}, 
+				{3, 4}, 
+				{5, 6}
+				};
+		for(int i=0; i< array.length; i++) {
+			for(int j=0; j<array[i].length; j++) {
+				System.out.print(array[i][j]+" ");
+			}
+			System.out.println();
+		}
+
+		
+	// Q8. 
+		// 2次元配列 int[][] array = {{10, 20, 30}, {40, 50, 60}, {70, 80, 90}}; を用意する
+		// 配列の すべての要素の合計値を表示
+		int[][] array1= {
+				{10,20,30},
+				{40,50,60},
+				{70,80,90}
+		};
+		int sum1=0;// 合計を入れる変数
 	
-
+		// すべての要素を取り出して表示
+		for(int i=0; i<array1.length; i++) { // 行のループ
+			for(int j = 0; j<array1[i].length; j++) { // 列のループ
+				System.out.print(array1[i][j]+" ");
+				sum1 += array1[i][j]; // → sum = sum + array[i][j]; と同じ意味
+				                      //→ 「今までの合計 sum に、今の要素 array[i][j] を足す」
+			}
+			System.out.println();
+		}
+		System.out.println("合計値 : "+sum1);
+		
+	// Q9. 
+		// 2次元配列 int[][] array = {{12, 15, 8}, {6, 19, 25}, {30, 2, 10}}; を用意する
+		// 配列内の 最大値と最小値 を求めて表示
+		int[][]array2= {
+				{12,15,8},
+				{6,19,25},
+				{30,2,10}
+		};
+		
+		int max1 =array2[0][0];  // 最大値の初期値
+		int min1 =array2[0][0];  // 最小値の初期値
+		
+		for(int i=0; i<array2.length; i++) {
+			for(int j=0; j<array2[i].length; j++) {
+				if(array2[i][j]>max1) {
+					max1= array2[i][j]; // 最大値更新
+				}
+				if(array2[i][j]<min1) {
+					min1= array2[i][j]; // 最小値更新
+				}
+			}
+		}
+		System.out.println("最大値: "+max1);
+		System.out.println("最小値: "+min1);
+	
+		
+	// Q10.
+		// 3次元配列 int[][][] array = {{{1, 2}, {3, 4}}, {{5, 6}, {7, 8}}}; を用意する
+		// 配列内のすべての要素を 表示
+		int[][][]array3= {
+			{{1,2}, {3,4}},
+			{{5,6}, {7,8}}
+		};
+		
+		// 3重ループで全要素を表示
+		for(int i=0; i<array3.length; i++) {
+			for(int j=0; j<array3[i].length; j++) {
+				for(int k=0; k<array3[i][j].length; k++) {
+					System.out.print(array3[i][j][k]+" ");
+				}
+			}
+			System.out.println(); // ブロックごとに改行
+		}
 	}
-
 }
--- HelloWorld_HImuraMayu/src/test_B/Review.java
+++ HelloWorld_HImuraMayu/src/test_B/Review.java
@@ -33,7 +33,80 @@ public class Review {
 			System.out.println(characters[i]);
 		}
 		
+		// 拡張for文（for-each 文）は、インデックスを意識せずに配列を処理 できます。
+		int[] counts= {9,10,11,19};
+		for(int num : counts) {
+			System.out.println(num);
+		}
 		
-
+		String name[] = {"sae", "rin", "ki"};
+		for(int i=0; i<name.length; i++) {
+			System.out.println(name[i]);
+		}
+		System.out.println("name[0]は"+name[0]);
+		System.out.println("name[1]は"+name[1]);
+		System.out.println("拡張for文");
+		// 配列 name の中から1つずつ nam に取り出して使う」という意味。
+		for(String nam: name) {
+			System.out.println(nam);
+		}
+		System.out.println("オリジナル");
+		
+		String name1[] = {"冴", "凛", "カイザー"};
+		for(int i=0; i<name1.length; i++) {
+			if(name1[i].equals("冴")) {
+				System.out.println("糸師冴");
+			}else{
+				System.out.println(name1[i]);
+			}
+		}
+		
+		int[][]Array= new int[2][3];
+		Array[0][0]=10;
+		Array[0][1] =20;
+		Array[0][2] =30;
+		Array[1][0]=40;
+		Array[1][1]=50;
+		Array[1][2]=60;
+		// for文で出力する例
+		for(int i=0; i<2; i++) { // 行
+			for(int j=0; j<3; j++) { // 列
+				System.out.print(Array[i][j]+" ");
+			}
+			System.out.println("");
+		}
+		
+		int[][]array={{1, 2}, {3, 4}, {5, 6}};
+		// 各行に2つずつ数値が入っています
+		System.out.println("アクセスの仕方（例）");
+		System.out.println(array[0][0]);
+		System.out.println(array[0][1]);
+		System.out.println(array[1][0]);
+		System.out.println(array[1][1]);
+		System.out.println(array[2][0]);
+		System.out.println(array[2][1]);
+		
+		System.out.println("for文ですべて出力する例");
+		for(int i=0; i<array.length;i++) {//array.length は 行数（3）
+			for(int j=0; j<array[i].length; j++) {// array[i].length は 列数（2）
+				System.out.print(array[i][j]+" ");
+			}
+			System.out.println();
+		}
+		
+		int[][][] array1={
+				{{1,2},{3,4}},
+				{{5,6},{7,8}}
+		};
+		System.out.println("0層/0行/0列 : "+array1[0][0][0]);
+		System.out.println("1層/1行/0列 : "+array1[1][1][0]);
+		for(int i=0; i<array1.length; i++) {
+			for(int j=0; j<array1[i].length; j++) {
+				for(int k=0; k<array1[i][j].length; k++) {
+					System.out.println("array1["+i+"]["+j+"]["+k+"]= "+array1[i][j][k]);
+				}
+				
+			}
+		}
 	}
 }
